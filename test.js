3a4768dd910b1ed1e34c8487fd1e210366c154cd
Raifatul Jannat Sheripta <132344220+Sheripta@users.noreply.github.com>
2024-06-29 02:16:29 +0600

    Add files via upload

---
+++ test.js
@@ -0,0 +1,51 @@
+console.log("Hello,World")
+alert("Javacscript is connected")
+  document.write("Welcome to Javacscript")
+ console.log("Welcome to Javacscript")
+ alert("Welcome to Javacscript")
+ 
+// JS syntax
+
+//  let x = 10;
+//  if (x>5) {
+//     console.log("x is geater than 5");
+//  }
+
+// Variables
+ const studentInfo={
+ firstname:"Jhon",
+  lastname:"Doe",
+  age:25,
+ }
+
+
+//  JS let
+
+ let x = 7;
+ {
+    let x=6;
+    console.log(x);
+ }
+ console.log(x);
+
+//  JS const
+
+
+// JS operators
+
+let y=8;
+let z=5;
+let sum=y+z;
+console.log(sum);
+let subtraction=y-z;
+console.log(subtraction);
+let multiplication=y*z;
+console.log(multiplication);
+let division=y/z;
+console.log(division);
+let modulus=y%z;
+console.log(modulus);
+
+
+// JS Arithmetic
+var a=Number(prompt("enter first number"));
