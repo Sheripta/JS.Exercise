d053c3d431da72db371546ea44a9a40cc3ad1bbc
Raifatul Jannat Sheripta <132344220+Sheripta@users.noreply.github.com>
2024-06-29 02:59:55 +0600

    Update test.js

--- test.js
+++ test.js
@@ -28,9 +28,6 @@ alert("Javacscript is connected")
  }
  console.log(x);
 
-//  JS const
-
-
 // JS operators
 
 let y=8;
@@ -49,3 +46,83 @@ console.log(modulus);
 
 // JS Arithmetic
 var a=Number(prompt("enter first number"));
+// JS Assignment
+let a=10;
+a+=5;
+console.log('value of a is:'+a);
+
+let a=10;
+a-=5;
+console.log('value of a is:'-a);
+
+
+let a=10;
+a*=5;
+console.log('value of a is:'*a);
+
+
+let a=10;
+a%=5;
+console.log('value of a is:'%a);
+
+// JS Data Types
+
+let a="John"
+console.log(typeof(a));
+let b=3;
+console.log(typeof(b));
+let c=true;
+console.log(typeof(c));
+let person={
+   firstname:"John",
+   lastname:"Doe",
+}
+console.log(typeof(person));
+
+let d =["3"]
+console.log(typeof(d));
+
+// JS Functions
+function sum(a,b){
+   return a+b;
+}
+const value =sum(1,4);
+console.log(value);
+
+// JS Objects
+
+const studentInfo={
+   firstname:"Jhon",
+    lastname:"Doe",
+    age:25,
+    grade:"5.00",
+   }
+console.log(studentInfo);
+
+
+
+// JS Object Properties
+
+// Adding properties using dot notation
+studentInfo.country = 'BD';
+studentInfo.email = 'jhon@example.com';
+ 
+// Adding properties using bracket notation
+studentInfo['hobby'] = 'Gaming';
+studentInfo['gender'] = 'Male';
+ 
+console.log('After adding properties:');
+console.log(studentInfo);
+ 
+
+// Removing properties using dot notation
+delete studentInfo.age;
+delete studentInfo.grade;
+ 
+// Removing properties using bracket notation
+delete studentInfo['hobby'];
+delete studentInfo['gender'];
+ 
+console.log('After removing properties:');
+console.log(studentInfo);
+
